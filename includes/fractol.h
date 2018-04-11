9cf2e4d78b3eba1c8fb35e6045486346ec6df1b7
Isabelle PORSENNA <iporsenn@e2r11p13.42.fr>
2018-04-11 15:03:04 +0200

    base

---
+++ includes/fractol.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iporsenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/11 14:55:13 by iporsenn          #+#    #+#             */
+/*   Updated: 2018/04/11 14:55:15 by iporsenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		FRACTOL_H
+# define	FRACTOL_H
+
+void	draw_segment(float *coord_src, float *coord_dst, t_fdf *global)
+int		main(int ac, char **av);
+
+# endif
